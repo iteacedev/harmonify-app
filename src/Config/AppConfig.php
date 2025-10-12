bc1776a30c70fc846fc44e5db00219b83fe4208e
Tamires <tamirescruztrabalho@gmail.com>
2025-10-12 11:20:07 -0300

    fix storage folder

--- src/Config/AppConfig.php
+++ src/Config/AppConfig.php
@@ -7,6 +7,13 @@ class AppConfig
 {
     public static function init()
     {
+        $storageFolderPath = __DIR__ . '/../../storage/';
+        $storageFolderExists = is_dir($storageFolderPath);
+
+        if (!$storageFolderExists) {
+            mkdir($storageFolderPath);
+        }
+
         date_default_timezone_set('America/Sao_Paulo');
         $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
         $dotenv->load();
